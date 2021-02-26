e5c2e526dde76eeee6bd9582629eb0430a854f01
andrejrakic <andrej.rakic@mvpworkshop.co>
2021-02-26 17:27:14 +0100

    Remove unnecessary libraries

--- contracts/TimeBasedSwitch.sol
+++ contracts/TimeBasedSwitch.sol
@@ -1,8 +1,6 @@
 pragma solidity ^0.6.0;
 
 import "./interfaces/ITimeBasedSwitch.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/utils/Pausable.sol";
 import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
