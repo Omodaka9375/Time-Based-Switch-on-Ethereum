0e0915355971be2d3543c363862833832224fd22
andrejrakic <andrej.rakic@mvpworkshop.co>
2021-04-07 03:07:48 +0200

    Add kovan addresses for supported erc20 tokens

--- app/ercTokens.js
+++ app/ercTokens.js
@@ -8,8 +8,10 @@ let ercTokens = {
     },
     "tokens": [
       {
-        "address": "0x514910771AF9Ca656af840dff83E8264EcF986CA",
-        "chainId": 1,
+        "address": "0xa36085f69e2889c224210f603d836748e7dc0088",
+        "chainId": 42,
+        // "address": "0x514910771AF9Ca656af840dff83E8264EcF986CA",
+        // "chainId": 1,
         "name": "ChainLink",
         "symbol": "LINK",
         "decimals": 18,
@@ -24,8 +26,10 @@ let ercTokens = {
         "logoURI": ""
       },
       {
-        "address": "0x1f9840a85d5af5bf1d1762f925bdaddc4201f984",
-        "chainId": 1,
+        "address": "0x1f9840a85d5aF5bf1D1762F925BDADdC4201F984",
+        "chainId": 42,
+        // "address": "0x1f9840a85d5af5bf1d1762f925bdaddc4201f984",
+        // "chainId": 1,
         "name": "Uniswap",
         "symbol": "UNI",
         "decimals": 18,
@@ -40,32 +44,40 @@ let ercTokens = {
         "logoURI": ""
       },
       {
-        "address": "0xdac17f958d2ee523a2206206994597c13d831ec7",
-        "chainId": 1,
+        "chainId": 42,
+        "address": "0x07de306FF27a2B630B1141956844eB1552B956B5",
+        // "address": "0xdac17f958d2ee523a2206206994597c13d831ec7",
+        // "chainId": 1,
         "name": "Tether USD",
         "symbol": "USDT",
         "decimals": 6,
         "logoURI": ""
       },
       {
-        "address": "0x6b175474e89094c44da98b954eedeac495271d0f",
-        "chainId": 1,
+        "chainId": 42,
+        "address": "0x4F96Fe3b7A6Cf9725f59d353F723c1bDb64CA6Aa",
+        // "address": "0x6b175474e89094c44da98b954eedeac495271d0f",
+        // "chainId": 1,
         "name": "DAI Stablecoin",
         "symbol": "DAI",
         "decimals": 18,
         "logoURI": ""
       },
       {
-        "address": "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48",
-        "chainId": 1,
+        "chainId": 42,
+        "address": "0xb7a4F3E9097C08dA09517b5aB877F7a917224ede",
+        // "address": "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48",
+        // "chainId": 1,
         "name": "USD Coin",
         "symbol": "USDC",
         "decimals": 6,
         "logoURI": ""
       },
       {
-        "address": "0xc011a73ee8576fb46f5e1c5751ca3b9fe0af2a6f",
-        "chainId": 1,
+        "address": "0x7fdb81b0b8a010dd4ffc57c3fecbf145ba8bd947",
+        "chainId": 42,
+        // "address": "0xc011a73ee8576fb46f5e1c5751ca3b9fe0af2a6f",
+        // "chainId": 1,
         "name": "Synthetix Network Token",
         "symbol": "SNX",
         "decimals": 18,
@@ -80,16 +92,20 @@ let ercTokens = {
         "logoURI": ""
       },
       {
-        "address": "0xf629cbd94d3791c9250152bd8dfbdf380e2a3b9c",
-        "chainId": 1,
+        "address": "0xf6fe970533fe5C63d196139B14522Eb2956f8621",
+        "chainId": 42,
+        // "address": "0xf629cbd94d3791c9250152bd8dfbdf380e2a3b9c",
+        // "chainId": 1,
         "name": "Enjin Coin",
         "symbol": "ENJ",
         "decimals": 18,
         "logoURI": ""
       },
       {
-        "chainId": 1,
-        "address": "0xaaAEBE6Fe48E54f431b0C390CfaF0b017d09D42d",
+        "chainId": 42,
+        "address": "0x21F50Cd19507d2fED78bc09A4404a8bfC4F7fD81",
+        // "chainId": 1,
+        // "address": "0xaaAEBE6Fe48E54f431b0C390CfaF0b017d09D42d",
         "name": "Celsius",
         "symbol": "CEL",
         "decimals": 4,
@@ -104,16 +120,20 @@ let ercTokens = {
         "logoURI": ""
       },
       {
-        "chainId": 1,
-        "address": "0xc00e94cb662c3520282e6f5717214004a7f26888",
+        "chainId": 42,
+        "address": "0x61460874a7196d6a22d1ee4922473664b3e95270",
+        // "chainId": 1,
+        // "address": "0xc00e94cb662c3520282e6f5717214004a7f26888",
         "name": "Compound",
         "symbol": "COMP",
         "decimals": 18,
         "logoURI": ""
       },
       {
-        "chainId": 1,
-        "address": "0x7d1afa7b718fb893db30a3abc0cfc608aacfebb0",
+        "chainId": 42,
+        "address": "0xf779f53281d65510aab94898ec943ab99d8e6f70",
+        // "chainId": 1,
+        // "address": "0x7d1afa7b718fb893db30a3abc0cfc608aacfebb0",
         "name": "Polygon",
         "symbol": "MATIC",
         "decimals": 18,
@@ -128,8 +148,10 @@ let ercTokens = {
         "logoURI": ""
       },
       {
-        "chainId": 1,
-        "address": "0xe41d2489571d322189246dafa5ebde1f4699f498",
+        "chainId": 42,
+        "address": "0x162c44e53097e7B5aaE939b297ffFD6Bf90D1EE3",
+        // "chainId": 1,
+        // "address": "0xe41d2489571d322189246dafa5ebde1f4699f498",
         "name": "0x Protocol Token",
         "symbol": "ZRX",
         "decimals": 18,
